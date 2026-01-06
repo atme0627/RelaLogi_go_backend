a90d84d5326d66affdb5c95793450885cb24a33c
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-07 01:54:35 +0900

    ginへの移行 - handler/healthの実装

---
+++ framework/adapter/gin/handler/health.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/atme0627/RelaLogi_go_backend/controller"
+	"github.com/atme0627/RelaLogi_go_backend/framework/adapter/response"
+	"github.com/gin-gonic/gin"
+)
+
+type HealthHandler struct {
+	c *controller.HealthController
+}
+
+func NewHandler(c *controller.HealthController) *HealthHandler {
+	return &HealthHandler{c: c}
+}
+
+func (h *HealthHandler) Register(r gin.IRoutes) {
+	r.GET("/health", h.get)
+}
+
+func (h *HealthHandler) get(ctx *gin.Context) {
+	resp, err := h.c.Get(ctx.Request.Context())
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, response.ErrorResponse{Message: "server error"})
+	}
+	ctx.JSON(http.StatusOK, resp)
+}
