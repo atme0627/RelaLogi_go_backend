46f4870e21c3cf21d8adb2b2a59fe0fda7cdbf26
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-06-17 00:58:40 +0900

    CropHintsFromImageのOpenCV実装

--- entity/puzzle.go
+++ entity/puzzle.go
@@ -1,5 +1,7 @@
 package entity
 
+import "math"
+
 type EncodedImage struct {
 	Bytes     []byte
 	MimeTypes string
@@ -9,11 +11,44 @@ type Point struct {
 	Y int
 }
 
+func (p *Point) Distance(q Point) float64 {
+	return math.Sqrt(float64((p.X-q.X)*(p.X-q.X) + (p.Y-q.Y)*(p.Y-q.Y)))
+}
+
 type Quad struct {
 	P1 Point
 	P2 Point
 	P3 Point
 	P4 Point
+}
+
+func (q *Quad) SortClockwiseFromTopLeft() {
+	var topLeft, topRight, bottomRight, bottomLeft Point
+	var maxSum, minSum, maxDiff, minDiff int
+	for _, p := range []Point{q.P1, q.P2, q.P3, q.P4} {
+		sum := p.X + p.Y
+		diff := p.Y - p.X
+		if sum > maxSum {
+			maxSum = sum
+			bottomRight = p
+		} else if sum < minSum {
+			minSum = sum
+			topLeft = p
+		}
+
+		if diff > maxDiff {
+			maxDiff = diff
+			bottomLeft = p
+		} else if diff < minDiff {
+			minDiff = diff
+			topRight = p
+		}
+	}
+
+	q.P1 = topLeft
+	q.P2 = topRight
+	q.P3 = bottomRight
+	q.P4 = bottomLeft
 }
 
 type PuzzleSize struct {
--- infra/puzzle/opencv_image_processer.go
+++ infra/puzzle/opencv_image_processer.go
@@ -1,16 +1,98 @@
 package infra
 
-import "github.com/atme0627/RelaLogi_go_backend/entity"
+import (
+	"image"
+
+	"github.com/atme0627/RelaLogi_go_backend/entity"
+
+	"gocv.io/x/gocv"
+)
 
 type OpenCVImageProcessor struct {
 }
 
-func (o OpenCVImageProcessor) CropHintsFromImage(image entity.EncodedImage, vHintQuad entity.Quad, hHintQuad entity.Quad) (entity.EncodedImage, entity.EncodedImage, error) {
-	//TODO implement me
-	panic("implement me")
+func (o OpenCVImageProcessor) CropHintsFromImage(img entity.EncodedImage, vHintQuad entity.Quad, hHintQuad entity.Quad) (entity.EncodedImage, entity.EncodedImage, error) {
+	closeMat := func(mat *gocv.Mat) {
+		err := mat.Close()
+		if err != nil {
+		}
+	}
+
+	mat, err := gocv.IMDecode(img.Bytes, gocv.IMReadColor)
+	if err != nil {
+		return entity.EncodedImage{}, entity.EncodedImage{}, err
+	}
+	defer closeMat(&mat)
+
+	vHintQuad.SortClockwiseFromTopLeft()
+	hHintQuad.SortClockwiseFromTopLeft()
+
+	vRectified := o.getRectifiedImageQuad(vHintQuad)
+	hRectified := o.getRectifiedImageQuad(hHintQuad)
+
+	pvVFrom := gocv.NewPointVectorFromPoints([]image.Point{image.Point(vHintQuad.P1), image.Point(vHintQuad.P2), image.Point(vHintQuad.P3), image.Point(vHintQuad.P4)})
+	pvHFrom := gocv.NewPointVectorFromPoints([]image.Point{image.Point(hHintQuad.P1), image.Point(hHintQuad.P2), image.Point(hHintQuad.P3), image.Point(hHintQuad.P4)})
+	pvVTo := gocv.NewPointVectorFromPoints([]image.Point{image.Point(vRectified.P1), image.Point(vRectified.P2), image.Point(vRectified.P3), image.Point(vRectified.P4)})
+	pvHTo := gocv.NewPointVectorFromPoints([]image.Point{image.Point(hRectified.P1), image.Point(hRectified.P2), image.Point(hRectified.P3), image.Point(hRectified.P4)})
+	defer pvVFrom.Close()
+	defer pvHFrom.Close()
+	defer pvVTo.Close()
+	defer pvHTo.Close()
+
+	vM := gocv.GetPerspectiveTransform(pvVFrom, pvVTo)
+	hM := gocv.GetPerspectiveTransform(pvHFrom, pvHTo)
+
+	vCroppedMatDest := gocv.NewMat()
+	hCroppedMatDest := gocv.NewMat()
+	defer closeMat(&vCroppedMatDest)
+	defer closeMat(&hCroppedMatDest)
+
+	err = gocv.WarpPerspective(mat, &vCroppedMatDest, vM, image.Point(vRectified.P3))
+	if err != nil {
+		return entity.EncodedImage{}, entity.EncodedImage{}, err
+	}
+	err = gocv.WarpPerspective(mat, &hCroppedMatDest, hM, image.Point(hRectified.P3))
+	if err != nil {
+		return entity.EncodedImage{}, entity.EncodedImage{}, err
+	}
+
+	vBuf, err := gocv.IMEncode(gocv.PNGFileExt, vCroppedMatDest)
+	if err != nil {
+		return entity.EncodedImage{}, entity.EncodedImage{}, err
+	}
+	defer vBuf.Close()
+	hBuf, err := gocv.IMEncode(gocv.PNGFileExt, hCroppedMatDest)
+	if err != nil {
+		return entity.EncodedImage{}, entity.EncodedImage{}, err
+	}
+	defer hBuf.Close()
+
+	vCroppedImage, err := entity.NewEncodedImage(vBuf.GetBytes(), img.MimeTypes)
+	if err != nil {
+		return entity.EncodedImage{}, entity.EncodedImage{}, err
+	}
+	hCroppedImage, err := entity.NewEncodedImage(hBuf.GetBytes(), img.MimeTypes)
+	if err != nil {
+		return entity.EncodedImage{}, entity.EncodedImage{}, err
+	}
+
+	return vCroppedImage, hCroppedImage, nil
 }
 
 func (o OpenCVImageProcessor) SplitHintToCells(hint entity.EncodedImage, size entity.PuzzleSize) [][]entity.EncodedImage {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (o OpenCVImageProcessor) getRectifiedImageQuad(hintQuad entity.Quad) entity.Quad {
+	// clockwiseでソート済みの想定
+	topWidth := hintQuad.P1.Distance(hintQuad.P2)
+	bottomWidth := hintQuad.P3.Distance(hintQuad.P4)
+	leftHeight := hintQuad.P1.Distance(hintQuad.P4)
+	rightHeight := hintQuad.P2.Distance(hintQuad.P3)
+
+	height := int(max(leftHeight, rightHeight))
+	width := int(max(topWidth, bottomWidth))
+
+	return entity.Quad{entity.Point{0, 0}, entity.Point{width, 0}, entity.Point{width, height}, entity.Point{0, height}}
 }
