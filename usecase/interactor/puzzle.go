4183534ce2fe86b1fa0a2dfe5531427dc7a495ae
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-06-25 01:19:28 +0900

    fromImageをリファクタリング

--- usecase/interactor/puzzle.go
+++ usecase/interactor/puzzle.go
@@ -18,69 +18,17 @@ func New(imageProcesser port.ImageProcessor, ocr port.OCR) *PuzzleInteractor {
 
 func (i PuzzleInteractor) FromImage(ctx context.Context, image entity.EncodedImage, vHintQuad entity.Quad, hHintQuad entity.Quad, size entity.PuzzleSize) (*entity.Puzzle, [2]entity.EncodedImage, error) {
 	const TRIM_PIXEL = 2
-
 	vHintImage, hHintImage, err := i.imageProcessor.CropHintsFromImage(image, vHintQuad, hHintQuad)
 	if err != nil {
 		return nil, [2]entity.EncodedImage{}, err
 	}
-
-	vHintCells := i.imageProcessor.SplitHintToCells(vHintImage, size)
-	hHintCells := i.imageProcessor.SplitHintToCells(hHintImage, size)
-
-	vHint := make([][]int, size.VHintHeight)
-	for j := range vHint {
-		vHint[j] = make([]int, size.Width)
+	vHint, err := i.recognizeHintFromImage(ctx, vHintImage, vHintQuad, size.VHintHeight, size.Width, TRIM_PIXEL)
+	if err != nil {
+		return nil, [2]entity.EncodedImage{}, err
 	}
-	for j, rows := range vHintCells {
-		for k, cell := range rows {
-			recognizedNumber := 0
-			preprocessedCells, err := i.imageProcessor.PreprocessAndSplitCellToDigits(cell, TRIM_PIXEL)
-			if err != nil {
-				return nil, [2]entity.EncodedImage{}, err
-			}
-
-			for _, preprocessedCell := range preprocessedCells {
-				ocrResult, err := i.ocr.RecognizeNumberFromCell(preprocessedCell)
-				if err != nil {
-					return nil, [2]entity.EncodedImage{}, err
-				}
-				recognizedNumber = recognizedNumber*10 + ocrResult
-				if recognizedNumber == -1 {
-					continue
-				}
-			}
-
-			vHint[j][k] = recognizedNumber
-		}
-
-	}
-
-	hHint := make([][]int, size.Height)
-	for j := range hHint {
-		hHint[j] = make([]int, size.HHintWidth)
-	}
-	for j, rows := range hHintCells {
-		for k, cell := range rows {
-			recognizedNumber := 0
-			preprocessedCells, err := i.imageProcessor.PreprocessAndSplitCellToDigits(cell, TRIM_PIXEL)
-			if err != nil {
-				return nil, [2]entity.EncodedImage{}, err
-			}
-
-			for _, preprocessedCell := range preprocessedCells {
-				ocrResult, err := i.ocr.RecognizeNumberFromCell(preprocessedCell)
-				if err != nil {
-					return nil, [2]entity.EncodedImage{}, err
-				}
-				recognizedNumber = recognizedNumber*10 + ocrResult
-				if recognizedNumber == -1 {
-					continue
-				}
-			}
-
-			vHint[j][k] = recognizedNumber
-		}
-
+	hHint, err := i.recognizeHintFromImage(ctx, hHintImage, hHintQuad, size.Height, size.HHintWidth, TRIM_PIXEL)
+	if err != nil {
+		return nil, [2]entity.EncodedImage{}, err
 	}
 
 	draftPuzzle := &entity.Puzzle{Size: size, VHint: vHint, HHint: hHint}
@@ -90,4 +38,35 @@ func (i PuzzleInteractor) FromImage(ctx context.Context, image entity.EncodedIma
 func (i PuzzleInteractor) Create(ctx context.Context) error {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (i PuzzleInteractor) recognizeHintFromImage(ctx context.Context, hintImage entity.EncodedImage, hintQuad entity.Quad, height int, width int, trimPixel int) ([][]int, error) {
+	vHintCells := i.imageProcessor.SplitHintToCells(hintImage, height, width)
+
+	result := make([][]int, height)
+	for j := range result {
+		result[j] = make([]int, width)
+	}
+	for j, rows := range vHintCells {
+		for k, cell := range rows {
+			recognizedNumber := 0
+			preprocessedCells, err := i.imageProcessor.PreprocessAndSplitCellToDigits(cell, trimPixel)
+			if err != nil {
+				return nil, err
+			}
+
+			for _, preprocessedCell := range preprocessedCells {
+				ocrResult, err := i.ocr.RecognizeNumberFromCell(preprocessedCell)
+				if err != nil {
+					return nil, err
+				}
+				recognizedNumber = recognizedNumber*10 + ocrResult
+				if recognizedNumber == -1 {
+					continue
+				}
+			}
+			result[j][k] = recognizedNumber
+		}
+	}
+	return result, nil
 }
